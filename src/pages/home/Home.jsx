31e4f4cf0e723e849ceac5417719e8a04efcdeb5
Torabis <sadihome@gmail.com>
2022-04-04 12:58:45 -0700

    create user, product, add pages

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import Widget from "../../components/widget/Widget";
 import Featured from "../../components/featured/Featured";
 import Chart from "../../components/chart/Chart";
+import Table from "../../components/table/Table";
 import "./home.scss";
 
 const Home = () => {
@@ -19,7 +20,11 @@ const Home = () => {
         </div>
         <div className="charts">
           <Featured />
-          <Chart/>
+          <Chart title="Last 6 Months" aspect={2 / 1}/>
+        </div>
+        <div className="listContainer">
+          <div className="listTitle">Latest Transactions</div>
+          <Table />
         </div>
       </div>
     </div>
